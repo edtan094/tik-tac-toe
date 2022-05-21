6a3d12f04dd4bb6e9eaa143fd5aa0fd00fe48ba3
edtan094 <etan094@gmail.com>
2022-05-20 21:47:22 -0700

    Was able to get the X and O's to render on the page.  Still need to work on getting the X and O's to appear in the approrpiate order when not clicking on the first column.

--- tic-tac-toe-react/src/App.jsx
+++ tic-tac-toe-react/src/App.jsx
@@ -1,20 +1,48 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 
 function App() {
+  const [player, setPlayer] = useState("X")
+  const [row1, setRow1] = useState([])
+  const [row2, setRow2] = useState([])
+  const [row3, setRow3] = useState([])
+
+
+  const userInput = (event) => {
+    console.log(player)
+    console.log(event.target.className)
+    console.log(row1)
+    if(event.target.className === "row1"){
+      setRow1([...row1, player])
+    }
+    if(event.target.className === "row2"){
+      setRow2([...row2, player])
+    }
+    if(event.target.className === "row3"){
+      setRow3([...row3, player])
+    }
+    if(player === "X"){
+      setPlayer("O")
+    }
+    if(player === "O"){
+      setPlayer("X")
+    }
+  }
+
   return (
     <>
       <div className='container'>
         <div className='row justify-center'>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
+          <button onClick={userInput} className='row1'>{row1[0] ? row1[0] : null}</button>
+          <button onClick={userInput} className='row1'>{row1[1] ? row1[1] : null}</button>
+          <button onClick={userInput} className='row1'>{row1[2] ? row1[2] : null}</button>
+          <button onClick={userInput} className='row2'>{row2[0] ? row2[0] : null}</button>
+          <button onClick={userInput} className='row2'>{row2[1] ? row2[1] : null}</button>
+          <button onClick={userInput} className='row2'>{row2[2] ? row2[2] : null}</button>
+          <button onClick={userInput} className='row3'>{row3[0] ? row3[0] : null}</button>
+          <button onClick={userInput} className='row3'>{row3[1] ? row3[1] : null}</button>
+          <button onClick={userInput} className='row3'>{row3[2] ? row1[2] : null}</button>
         </div>
       </div>
     </>
